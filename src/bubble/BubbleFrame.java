5c08c4ab8df3fcd2f99c3dbb083354152ffacba4
jsh940517 <jsh940517@daum.net>
2022-02-03 07:09:04 +0900

    first commit

---
+++ src/bubble/BubbleFrame.java
@@ -0,0 +1,18 @@
+package bubble;
+
+import javax.swing.JFrame;
+
+public class BubbleFrame  extends JFrame{
+	
+	public BubbleFrame() {
+		// TODO Auto-generated constructor stub
+		setSize(1000, 640);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BubbleFrame();
+	}
+
+}
